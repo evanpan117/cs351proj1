be32ba8960f68ba4f807abf2fa1df7e164acff09
Evan Pan <haojie@csu.fullerton.edu>
2019-02-08 22:04:23 -0800

    update

--- time_shm.c
+++ time_shm.c
@@ -13,18 +13,18 @@ int main(int argc, char *argv[])
   // string ls = "ls";
   // args [0] = (char*)ls.c_str();
   // args [1] = NULL;
-struct timeval;
+struct timeval_t;
 /* create the shared memory object */
 int fd = shm_open("Command Timer",O_CREAT | O_RDWR,0666);
 /* configure the size of the shared memory object */
 ftruncate(fd, sizeof(timeval_t));
 /* memory map the shared memory object */
-timeval * sharedMemory = mmap(0, sizeof(timeval_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+timeval_t * sharedMemory = mmap(0, sizeof(timeval_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 /* write to the shared memory object */
 
 if( fork() == 0 )
 {
-  timeval startTime;
+  timeval_t startTime;
   gettimeofday( &startTime, 0 );
   sprintf(sharedMemory,"%l",startTime);
   sharedMemory += sizeof(startTime);
@@ -32,10 +32,10 @@ if( fork() == 0 )
 else
 {
   execvp( argc[1], argv +1)
-  timeval end_time;
+  timeval_t end_time;
   gettimeofday( &end_time, 0 );
 
-  timeval elapsed_time;
+  timeval_t elapsed_time;
   timersub( &end_time, &startTime, &elapsed_time );
   /* read from the shared memory object */
   printf("%l",sharedMemory);
